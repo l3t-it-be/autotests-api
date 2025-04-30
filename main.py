3ab644cd02b7f310c40904993100b3cc6e03fe72
KateBelova <ka7e.online@gmail.com>
2025-04-30 22:15:31 +0300

    4.2 Публикация проекта на GitHub. Шаг 10: Практика работы с GitHub

---
+++ main.py
@@ -0,0 +1,23 @@
+def welcome():
+    print(
+        f'Here’s to the crazy ones.\nThe misfits.\nThe rebels.\nThe troublemakers.\n\n'
+        f'The round pegs in the square holes.\n\n'
+        f'They’re not fond of rules.\n'
+        f'And they have no respect for the status quo.\n\n'
+        f'You can quote them, disagree with them, glorify or vilify them.\n'
+        f'But the only thing you can’t do is ignore them.\n\n'
+        f'Because they change things.\n'
+        f'They invent. They imagine. They heal. They explore. They create. They inspire.\n'
+        f'They push the human race forward.\n\n'
+        f'Maybe they have to be crazy.\n\n'
+        f'How else can you stare at an empty canvas and see a work of art?\n'
+        f'Or sit in silence and hear a song that’s never been written?\n'
+        f'Or gaze at a red planet and see a laboratory on wheels?\n\n'
+        f'While some see them as the crazy ones, we see genius.\n'
+        f'Because the people who are crazy enough to think they can change the world, '
+        f'are the ones who do.'
+    )
+
+
+if __name__ == '__main__':
+    welcome()
